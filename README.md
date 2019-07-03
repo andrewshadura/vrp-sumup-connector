b27009aa1b862ceee0bd1f218e4d39cca7b8ea3d
Andrej Shadura <andrew@shadura.me>
2019-07-03 13:07:42 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,49 @@
+VRP SumUp connector
+===================
+
+![Feature graphic](feature-graphic.svg)
+
+This utility application connects the SumUp app with the Slovak VRP
+virtual till app, allowing you to complete the card payment while issuing
+the receipt. The amount will automatically appear in SumUp without the
+need to type it in manually.
+
+To install this app, visit its page on [Google Play][0]
+
+To find out more about VRP, see this page (in Slovak): [Virtuálna registračná
+pokladnica][1]. The application itself is available from [Google Play][2].
+
+[SumUp][3] is a brand of a lightweight mobile payment terminal by SumUp Payments
+Limited. The application is as well available from [Google Play][4]
+
+ [0]: https://play.google.com/store/apps/details?id=me.shadura.vrpconnector
+ [1]: https://www.financnasprava.sk/sk/podnikatelia/dane/ekasa/vrp
+ [2]: https://play.google.com/store/apps/details?id=sk.financnasprava.vrp
+ [3]: https://sumup.co.uk/
+ [4]: https://play.google.com/store/apps/details?id=com.kaching.merchant
+
+License
+-------
+
+Copyright (C) 2019 Andrej Shadura
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the X11/MIT license as stated below.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ app/src/main/java/me/shadura/vrpconnector/MainActivity.kt
@@ -0,0 +1,156 @@
+package me.shadura.vrpconnector
+
+/* Copyright (C) 2019 Andrej Shadura */
+/* SPDX-License-Identifier: MIT */
+
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import java.math.BigDecimal
+
+fun Context.installApp(packageName: String) {
+    try {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.Builder().run {
+            scheme("market")
+            authority("details")
+            appendQueryParameter("id", packageName)
+            build()
+        }))
+    } catch (e: ActivityNotFoundException) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.Builder().run {
+            scheme("https")
+            authority("play.google.com")
+            path("store/apps/details")
+            appendQueryParameter("id", packageName)
+            build()
+        }))
+    }
+}
+
+object VrpUriBuilder {
+    private fun builder(): Uri.Builder =
+        Uri.Builder().run {
+            scheme("sk.financnasprava.vrp.app")
+            authority("response")
+        }
+
+    fun success(transactionId: String) =
+            builder().apply {
+                appendQueryParameter("state", "APPROVED")
+                appendQueryParameter("transactionId", transactionId)
+            }
+
+    fun failure() =
+            builder().apply {
+                appendQueryParameter("state", "ERROR")
+            }
+
+    fun declined() =
+        builder().apply {
+            appendQueryParameter("state", "DECLINED")
+        }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    class SumUpNotFoundDialog : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return activity?.run {
+                AlertDialog.Builder(this).run {
+                    setMessage(getString(R.string.no_sumup))
+                    setTitle(getString(R.string.cant_complete_payment))
+                    setPositiveButton(getString(R.string.go_back)) { dialog, _ ->
+                        dialog.dismiss()
+
+                        val vrpUri = VrpUriBuilder.failure().build()
+                        Log.i(TAG, "returning to VRP: $vrpUri")
+                        try {
+                            startActivity(Intent(Intent.ACTION_VIEW, vrpUri))
+                        } catch (e: ActivityNotFoundException) {
+                            Log.e(TAG, "can’t call VRP")
+                        }
+                    }
+                    setNeutralButton(getString(R.string.install_sumup)) { dialog, _ ->
+                        dialog.dismiss()
+
+                        installApp("com.kaching.merchant")
+                    }
+                    create()
+                }
+            } ?: throw IllegalStateException("Activity cannot be null")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        intent?.data?.also { uri: Uri ->
+            when (intent.action) {
+                Intent.ACTION_VIEW -> {
+                    when (uri.scheme) {
+                        "com.aevi.payment" -> {
+                            Log.i(TAG, "$uri")
+                            if (uri.authority == "purchase") {
+                                val amount = BigDecimal(uri.getQueryParameter("amount") ?: "0")
+                                val builder = Uri.Builder().apply {
+                                    scheme("sumupmerchant")
+                                    authority("pay")
+                                    path("1.0")
+                                    appendQueryParameter("affiliate-key", BuildConfig.sumupApiKey)
+                                    appendQueryParameter("app-id", BuildConfig.APPLICATION_ID)
+                                    appendQueryParameter("total", (amount / BigDecimal(100)).toPlainString())
+                                    appendQueryParameter("skip-screen-success", "true")
+                                    appendQueryParameter("currency", "EUR")
+                                    appendQueryParameter("callback", "me.shadura.vrpconnector.sumup://result")
+                                }
+                                val sumupUri = builder.build()
+                                Log.i(TAG, "invoking SumUp: $sumupUri")
+                                try {
+                                    startActivity(Intent(Intent.ACTION_VIEW, sumupUri))
+                                } catch (e: ActivityNotFoundException) {
+                                    SumUpNotFoundDialog().show(supportFragmentManager, "NoSumup")
+                                }
+                            }
+                        }
+                        "me.shadura.vrpconnector.sumup" -> {
+                            Log.i(TAG, "$uri")
+                            if (uri.authority == "result") {
+                                val vrpUri = when (uri.getQueryParameter("smp-status")) {
+                                    "success" ->
+                                        VrpUriBuilder.success(uri.getQueryParameter("smp-tx-code") ?: "")
+                                    else -> {
+                                         when (uri.getQueryParameter("smp-failure-cause")) {
+                                            "transaction-failed" ->
+                                                VrpUriBuilder.declined()
+                                            else ->
+                                                VrpUriBuilder.failure()
+                                        }
+                                    }
+                                }.build()
+
+                                Log.i(TAG, "calling back VRP: $vrpUri")
+                                try {
+                                    startActivity(Intent(Intent.ACTION_VIEW, vrpUri))
+                                } catch (e: ActivityNotFoundException) {
+                                    Log.e(TAG, "can’t call VRP")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val TAG = "VRP-SumUp"
+    }
+}
